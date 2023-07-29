08d60e8fc3ce5091b5b0e9d9281a957ab55d548e
AIsphere-ai <134244428+AIsphere-ai@users.noreply.github.com>
2023-07-29 16:26:52 +0900

    first commit
    Co-authored-by: shironeko3 <shirodev543@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('meimei-ai.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from meimei-ai!');
+		vscode.window.showInformationMessage('Hello World from meimei_ai!');
 	});
 
 	context.subscriptions.push(disposable);
