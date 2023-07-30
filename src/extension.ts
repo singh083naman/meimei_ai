084e32ba62a12761c823ba73b8a3720cf2357714
shironeko3 <shirodev543@gmail.com>
2023-07-30 12:29:34 +0900

    ユーザが選択したコード取得、出力候補表示テスト

--- src/extension.ts
+++ src/extension.ts
@@ -8,19 +8,66 @@ import fetch from "node-fetch";
 import { ChatGPTAPI } from "chatgpt";
 
 const example = async () => {
-  if (!process.env.OPENAI_API_KEY) {
-    return "APIキーが設定されていません。";
+  // console.log("入った", process.env.OPENAI_API_KEY);
+  // if (!process.env.OPENAI_API_KEY) {
+  //   return "APIキーが設定されていません。";
+  // }
+  const editor = vscode.window.activeTextEditor;
+  console.log("エディター", editor);
+
+  if (editor) {
+    let document = editor.document;
+    console.log("document", document);
+    let selection = editor.selection;
+
+    // 選択範囲のテキストを取得
+    const text = document.getText(selection);
+    console.log("選択テキスト", text);
+
+    // 選択範囲のテキストを表示
+    vscode.window.showInformationMessage(`Selected text: ${text}`);
+
+    try {
+      const api = new ChatGPTAPI({
+        apiKey: "自分のAPIキー",
+      });
+      const prompt = `
+        以下の関数の名前の候補を３つ以上考えてください。
+        ###
+          ${text}
+        ###
+        ###
+          候補は以下のjson形式で出力して下さい。
+          {
+            "nameType": "method",
+            "result": string[]
+          }";
+        ###
+      `;
+      const res = await api.sendMessage(prompt);
+      const obj = JSON.parse(res.text);
+      console.log("オブジェ", obj);
+      const array = obj.result;
+
+      // 関数名の候補をユーザに提示し、選択させる
+      let pickedFunctionName = await vscode.window.showQuickPick(array, {
+        placeHolder: "Choose a new function name",
+      });
+      console.log("選択内容", pickedFunctionName);
+
+      if (pickedFunctionName) {
+        // ユーザが選択した関数名で元のテキストを置換
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, pickedFunctionName!);
+        });
+      }
+      return res.text;
+    } catch (e) {
+      console.log("エラー", e);
+      return "";
+    }
   }
-  try {
-    const api = new ChatGPTAPI({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    const res = await api.sendMessage("恵比寿のおすすめのお店を教えて");
-    return res.text;
-  } catch (e) {
-    console.log("エラー", e);
-    return "";
-  }
+  return "";
 };
 
 // This method is called when your extension is activated
