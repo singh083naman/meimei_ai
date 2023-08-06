91a4fc26c9d092511f76f20ee51aef918d021ed4
naoki <naoki@example.com>
2023-08-06 11:00:31 +0900

    fix/error-fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-const config = vscode.workspace.getConfiguration("mai-ai.settings");
+const config = vscode.workspace.getConfiguration("mai-ai.settings").get;
 
 export type FetchGptResponse = {
   result: string[];
